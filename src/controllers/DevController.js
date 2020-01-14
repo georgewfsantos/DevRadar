dc20b212c163f40f7d69cc3dadb2efa4b712bfc5
George Santos <georgesantos@MacBook-Pro-de-George.local>
2020-01-14 16:21:17 -0300

    first commit

---
+++ src/controllers/DevController.js
@@ -0,0 +1,44 @@
+const axios = require("axios");
+const Dev = require("../models/Dev");
+const parseStringAsArray = require("../utils/parseStringAsArray");
+
+module.exports = {
+  async index(req, res) {
+    const devs = await Dev.find();
+    return res.json(devs);
+  },
+
+  async store(req, res) {
+    const { github_username, techs, latitude, longitude } = req.body;
+
+    let dev = await Dev.findOne({ github_username });
+
+    if (!dev) {
+      const response = await axios.get(
+        `https://api.github.com/users/${github_username}`
+      );
+
+      // name = login -> if name, then return name; if not, then return login;
+      const { name = login, avatar_url, bio } = response.data;
+
+      const techsArray = parseStringAsArray(techs);
+      const location = {
+        type: "Point",
+        coordinates: [longitude, latitude]
+      };
+
+      dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location
+      });
+    } else {
+      res.status(400).json("This username is already taken");
+    }
+
+    return res.json(dev);
+  }
+};
