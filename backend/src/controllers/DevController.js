9e0e7c3dd2ed5f1065de60d2c0d8d4f3e6c32d0c
George Santos <georgesantos@MacBook-Pro-de-George.local>
2020-01-17 16:01:35 -0300

    implement real-time communication

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 const Dev = require("../models/Dev");
 const parseStringAsArray = require("../utils/parseStringAsArray");
+const { findConnections, sendMessage } = require("../websocket");
 
 module.exports = {
   async index(req, res) {
@@ -35,6 +36,16 @@ module.exports = {
         techs: techsArray,
         location
       });
+
+      const sendSocketMessageTo = findConnections(
+        {
+          latitude,
+          longitude
+        },
+        techsArray
+      );
+
+      sendMessage(sendSocketMessageTo, "new-dev", dev);
     } else {
       res.status(400).json("This username is already taken");
     }
